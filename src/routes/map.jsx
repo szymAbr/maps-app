2bf1d4c3873e7b34886088ee7eb130e4ce434c46
szymAbr <sz.abramowski@gmail.com>
2022-05-19 21:11:01 +0200

    add interactive map

--- src/routes/map.jsx
+++ src/routes/map.jsx
@@ -2,10 +2,12 @@ import { useContext } from "react";
 import styled from "styled-components";
 import { GlobalContext } from "../context/GlobalState";
 import axios from "axios";
-import TestMap from "../components/TestMap";
+import MapMain from "../components/MapMain";
 
 const FlexDiv = styled.div`
   display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 export default function Map() {
@@ -13,27 +15,11 @@ export default function Map() {
 
   console.log(coordinates);
 
-  // function route() {
-  //   axios
-  //     .get(
-  //       `https://router.hereapi.com/v8/routes?transportMode=car&origin=52.5308,13.3847&destination=52.5264,13.3686&return=summary&apikey=${process.env.REACT_APP_API_KEY}`
-  //     )
-  //     .then((response) => {
-  //       const data = response.data;
-
-  //       console.log(data);
-  //     })
-  //     .catch((error) => {
-  //       const errorMsg = error.message;
-  //       console.log(errorMsg);
-  //     });
-  // }
-
   return (
     <FlexDiv>
       <h2>Map with selected route</h2>
 
-      <TestMap />
+      <MapMain />
     </FlexDiv>
   );
 }
