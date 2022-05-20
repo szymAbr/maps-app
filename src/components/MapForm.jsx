eb36c485686acb507cab767821c18122da3b710f
szymAbr <sz.abramowski@gmail.com>
2022-05-20 23:20:02 +0200

    add handleInput function

--- src/components/MapForm.jsx
+++ src/components/MapForm.jsx
@@ -1,13 +1,25 @@
 import { Form } from "./styles/MapForm.styled";
 
 export default function MapForm({ setPrice }) {
+  function handleInput(e) {
+    const price = e.target.value;
+
+    if (price >= 0 && price <= 1000) {
+      setPrice(price);
+    } else if (price < 0) {
+      alert("The proposed value is too low.");
+    } else {
+      alert("The proposed value is too high.");
+    }
+  }
+
   return (
     <>
       <Form>
         <input
           type="number"
-          placeholder="price per km [EUR]"
-          onInput={(e) => setPrice(e.target.value)}
+          placeholder="Price per km [EUR]"
+          onInput={handleInput}
         />
       </Form>
     </>
