becb643e8194354e1bd86f683066f6188ef32a12
szymAbr <sz.abramowski@gmail.com>
2022-05-22 02:22:03 +0200

    delete redundant code

--- src/routes/map.jsx
+++ src/routes/map.jsx
@@ -11,10 +11,6 @@ const FlexDiv = styled.div`
 `;
 
 export default function Map() {
-  const { coordinates } = useContext(GlobalContext);
-
-  console.log(coordinates);
-
   return (
     <FlexDiv>
       <h2>Map with selected route</h2>
